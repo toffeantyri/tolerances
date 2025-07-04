cfa67407739ac4dc9fd1381be928196cd2cd31d4
toffeantyri <toffeatyri@gmail.com>
2025-07-04 22:59:44 +0500

    dialog result - add named fields

--- composeApp/src/commonMain/kotlin/ru/tolerances/app/ui/tolerances_screen/ToleranceResultDialogView.kt
+++ composeApp/src/commonMain/kotlin/ru/tolerances/app/ui/tolerances_screen/ToleranceResultDialogView.kt
@@ -1,6 +1,7 @@
 package ru.tolerances.app.ui.tolerances_screen
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -23,8 +24,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import ru.tolerances.app.components.tolerances_screen_component.dialogs.ITolerancesResultDialogComponent
+import ru.tolerances.app.ui.theme.MintGreen
 import ru.tolerances.app.ui.theme.OceanBlue
 import ru.tolerances.app.ui.theme.bold16TextStyle
+import ru.tolerances.app.ui.theme.regular14TextStyle
 
 @Composable
 fun ToleranceResultDialogView(component: ITolerancesResultDialogComponent) {
@@ -44,51 +47,86 @@ fun ToleranceResultDialogView(component: ITolerancesResultDialogComponent) {
                 val halfToler = ((result.maxToler ?: 0f) + (result.minToler ?: 0f)) / 2
                 println("halfToler $halfToler")
                 val middleResult = component.inputedSize.value + (halfToler)
-                Column {
-                    if (result.maxToler == null || result.minToler == null) {
-                        Text("-")
-                    } else {
-                        SelectionContainer {
-                            Column {
-                                Row(
-                                    modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
-                                    horizontalArrangement = Arrangement.Center
-                                ) {
-                                    Text(result.maxToler.toString(), style = bold16TextStyle())
-                                }
-                                Row(
-                                    modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
-                                    horizontalArrangement = Arrangement.Center
-                                ) {
-                                    Text(
-                                        text = result.minToler.toString(),
-                                        style = bold16TextStyle()
-                                    )
-                                }
-                            }
-                        }
+
+                SelectionContainer {
+                    Column {
+
                         Row(
-                            modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
-                                .border(BorderStroke(1.dp, OceanBlue), RoundedCornerShape(4.dp))
-                                .padding(vertical = 8.dp)
-                                .padding(horizontal = 16.dp),
-                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
                             horizontalArrangement = Arrangement.Center
                         ) {
-                            Text(text = "Середина допуска: ")
-                            SelectionContainer {
-                                Text(
-                                    text = middleResult.toString(),
-                                    style = TextStyle(
-                                        fontSize = 24.sp,
-                                        fontWeight = FontWeight.Bold,
-                                        lineHeight = 18.2.sp,
-                                        letterSpacing = 0.42.sp
-                                    ),
-                                    maxLines = 1
-                                )
-                            }
+                            Text(
+                                text = "Номинальный размер: ",
+                                style = regular14TextStyle()
+                            )
+                            Text(
+                                modifier = Modifier.padding(horizontal = 16.dp),
+                                text = component.inputedSize.value.toString(),
+                                style = bold16TextStyle()
+                            )
+                        }
 
+                        if (result.maxToler == null || result.minToler == null) {
+                            Text("-")
+                        } else {
+                            SelectionContainer {
+                                Column {
+                                    Row(
+                                        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+                                        horizontalArrangement = Arrangement.Center
+                                    ) {
+                                        Text(
+                                            text = "Верхнее отклонение: ",
+                                            style = regular14TextStyle()
+                                        )
+                                        Text(
+                                            modifier = Modifier.padding(horizontal = 8.dp),
+                                            text = result.maxToler.toString(),
+                                            style = bold16TextStyle()
+                                        )
+                                    }
+                                    Row(
+                                        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+                                        horizontalArrangement = Arrangement.Center
+                                    ) {
+                                        Text(
+                                            text = "Нижнее отклонение: ",
+                                            style = regular14TextStyle()
+                                        )
+                                        Text(
+                                            modifier = Modifier.padding(horizontal = 8.dp),
+                                            text = result.minToler.toString(),
+                                            style = bold16TextStyle()
+                                        )
+                                    }
+                                }
+                            }
+                            Row(
+                                modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
+                                    .border(BorderStroke(1.dp, OceanBlue), RoundedCornerShape(4.dp))
+                                    .padding(vertical = 8.dp)
+                                    .padding(horizontal = 16.dp),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                Text(text = "Середина допуска: ")
+                                SelectionContainer {
+                                    Text(
+                                        modifier = Modifier.background(
+                                            MintGreen, RoundedCornerShape(4.dp)
+                                        ).padding(4.dp),
+                                        text = middleResult.toString(),
+                                        style = TextStyle(
+                                            fontSize = 24.sp,
+                                            fontWeight = FontWeight.Bold,
+                                            lineHeight = 18.2.sp,
+                                            letterSpacing = 0.42.sp
+                                        ),
+                                        maxLines = 1
+                                    )
+                                }
+
+                            }
                         }
                     }
                 }
